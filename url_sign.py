1352142d70c74354ff24c87028ca85a92bb8be9e
Frank Gruellich <frank.gruellich@here.com>
2014-07-17 13:02:27 +0200

    added url signing wrapper

---
+++ url_sign.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+
+from boto.s3.connection import S3Connection
+from argparse import ArgumentParser
+
+parser = ArgumentParser()
+parser.add_argument('-b', '--bucket', type=str, required=True)
+parser.add_argument('keys', nargs='+', type=str)
+
+args = parser.parse_args()
+
+s3_conn = S3Connection()
+
+for k in args.keys:
+    print s3_conn.generate_url(60, 'GET', args.bucket, k, force_http=True)
